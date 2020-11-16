9e448a7a231bd80a52c96670dc584309198ca1dd
Chelsea <croland711@yandex.com>
2020-11-15 17:12:01 -0800

    commit tree c56f6f064fab5776bc2eaef49bf1f3b618cc86bc

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+    array.sort
 
+end
+def sort_array_desc(array)
+    array.sort do | a, b|
+    b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+      a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+ end
+
+ def reverse_array(array)
+    array[0], array[1], array[2], = array[2], array[1], array[0]
+ end
+ def kesha_maker(array)
+    array.each do |name|
+      name[2] = "$"
+    end
+end
+
+def find_a(array)
+        array.find_all do |word|
+          word[0] == "a"
+        end
+end
+
+def sum_array(array)
+    array.inject(0){|sum,x| sum + x }
+end
+
+def add_s(array)
+    array.collect do |word|
+      if array[1] == word
+        word
+      else
+        word + "s"
+      end
+    end
+end
